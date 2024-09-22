239c852a4d658d3c755690e2fe27d3f881e75f84
Tu <52100138@student.tdtu.edu.vn>
2024-09-22 22:50:30 +0700

    Update

--- hello/social-todo-list/main.go
+++ hello/social-todo-list/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"main/common"
+	"main/middleware"
 	"main/modules/item/models"
 	"main/modules/item/transport/ginitem"
 	"net/http"
@@ -16,35 +17,26 @@ import (
 	"gorm.io/gorm"
 )
 
+func main() {
 
-  
-
-
-
-
-func main(){
-
-	err:=godotenv.Load()
+	err := godotenv.Load()
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
-	  }
-	PORT:=os.Getenv("PORT")
-	DB_CONN_STR:=os.Getenv("DB_CONN_STR")
-
+	}
+	PORT := os.Getenv("PORT")
+	DB_CONN_STR := os.Getenv("DB_CONN_STR")
 
 	// https://github.com/jackc/pgx
-	///Connect PostgreSQL 
+	///Connect PostgreSQL
 	dsn := DB_CONN_STR
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-	if err!=nil{
+	if err != nil {
 		log.Fatalln(err)
 	}
 
 	fmt.Println(db)
-
-	
 
 	// now:=time.Now().UTC()
 
@@ -60,7 +52,6 @@ func main(){
 	// ///Convert JSONData =byte[],err
 	// jsonData,err:= json.Marshal(item)
 
-
 	// if err!=nil{
 	// 	fmt.Println(err)
 	// 	return
@@ -68,9 +59,6 @@ func main(){
 
 	// ///Convert to JSON
 	// fmt.Println(string(jsonData))
-
-
-
 
 	// var item2 TodoItem
 
@@ -81,103 +69,97 @@ func main(){
 
 	// fmt.Println(item2)
 
-
 	/////For
-	sum:=0
-	for i:=0; i<10 ; i++ {
-		sum+=i
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
 	}
 
-	fmt.Printf("Sum: %d\n",sum)
-
+	fmt.Printf("Sum: %d\n", sum)
 
 	///While in Go
-	sum= 1
-	for sum<1000{
-		sum+=sum
+	sum = 1
+	for sum < 1000 {
+		sum += sum
 	}
 
-	fmt.Println("Sum:",sum)
+	fmt.Println("Sum:", sum)
 
-
-	var pow =[]int{1,2,3,4,5}
-	for i,value :=range pow{
-		fmt.Printf("Index: %d,Value: %d\n",i,value)
+	var pow = []int{1, 2, 3, 4, 5}
+	for i, value := range pow {
+		fmt.Printf("Index: %d,Value: %d\n", i, value)
 	}
-	
 
 	///CRUD:Create,Read,Update,Delete
-	
 
 	r := gin.Default()
 
-	v1:=r.Group("/v1")
+	// r.Use(middleware.Recovery())
+
+	v1 := r.Group("/v1")
 	{
-		items:=v1.Group("/items")
+		items := v1.Group("/items", middleware.Recovery())
 		{
-			items.POST("",ginitem.CreateItem(db))
-			items.GET("",getListItem(db))
-			items.GET("/:id",ginitem.GetItem(db))
-			items.PATCH("/:id",ginitem.UpdateItem(db))
-			items.DELETE("/:id",ginitem.DeleteItem(db))
+			items.POST("", ginitem.CreateItem(db))
+			items.GET("", getListItem(db))
+			items.GET("/:id", ginitem.GetItem(db))
+			items.PATCH("/:id", ginitem.UpdateItem(db))
+			items.DELETE("/:id", ginitem.DeleteItem(db))
 		}
 	}
 
-  	r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "Hello World",
-    })
-  })
-  r.Run(PORT) // listen and serve on 0.0.0.0:3000(for windows "localhost:3000")
+	r.GET("/ping", func(c *gin.Context) {
+		go func() {
+			defer common.Recovery()
+			fmt.Println([]int{}[0])
+		}()
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+	r.Run(PORT) // listen and serve on 0.0.0.0:3000(for windows "localhost:3000")
 
-
-	
 }
 
+func getListItem(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
 
+		var paging common.Paging
 
-func getListItem(db *gorm.DB) func(*gin.Context){
-	return func(c *gin.Context){
-
-		var paging  common.Paging
-		
-		if err:=c.ShouldBind(&paging);err!=nil{
-			c.JSON(http.StatusBadRequest,gin.H{
-				"error":err.Error(),
+		if err := c.ShouldBind(&paging); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
 			})
 
-			return 
+			return
 		}
 
 		paging.Process()
 
-		var result [] models.TodoItem
+		var result []models.TodoItem
 
+		db = db.Where("status <> ?", "Delete")
 
-		db = db.Where("status <> ?","Delete")
-
-		if	err:=db.Table(models.TodoItem{}.TableName()).
-		Count(&paging.Total).Error;err!=nil{
-			c.JSON(http.StatusBadRequest,gin.H{
-				"error":err.Error(),
+		if err := db.Table(models.TodoItem{}.TableName()).
+			Count(&paging.Total).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
 			})
 
-			return 
+			return
 		}
 
-		if err:=db.Order("id desc").
-		Offset((paging.Page-1)*paging.Limit).
-		Limit(paging.Limit).
-		Find(&result).Error;err!=nil{
-			c.JSON(http.StatusBadRequest,gin.H{
-				"error":err.Error(),
+		if err := db.Order("id desc").
+			Offset((paging.Page - 1) * paging.Limit).
+			Limit(paging.Limit).
+			Find(&result).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
 			})
 
-			return 
+			return
 		}
 
-		
-
-		c.JSON(http.StatusOK,common.NewSuccessResponse(result,paging,nil))
+		c.JSON(http.StatusOK, common.NewSuccessResponse(result, paging, nil))
 	}
 }
